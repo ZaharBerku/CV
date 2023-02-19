80b5636a33f399e964dacc0256bfb5b503c0f215
ZaharBerku <zaharberku@gmail.com>
2023-02-19 22:32:59 +0200

    typography

---
+++ src/components/atoms/Typography/index.constants.ts
@@ -0,0 +1,13 @@
+import type { Variants } from "./index.types";
+
+const variantsTypography: Record<Variants, string> = {
+  h1: "h1",
+  h2: "h2",
+  h3: "h3",
+  h4: "h4",
+  h5: "h5",
+  h6: "h6",
+  p: "p",
+};
+
+export { variantsTypography };
