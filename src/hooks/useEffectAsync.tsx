1a0cbfad13ba7493a85fdac18ff46f54d1ea727c
ZaharBerku <zaharberku@gmail.com>
2023-03-10 23:16:34 +0200

    added hook useEffectAsync

---
+++ src/hooks/useEffectAsync.tsx
@@ -0,0 +1,13 @@
+import { useEffect, DependencyList } from "react";
+
+const useEffectAsync = (effect: () => void, inputs: DependencyList) => {
+  useEffect(() => {
+    try {
+      effect();
+    } catch (err) {
+      console.log(err);
+    }
+  }, inputs);
+};
+
+export { useEffectAsync };
