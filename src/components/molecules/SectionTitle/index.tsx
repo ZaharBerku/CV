b08220ede53b1651a43db19ecad5ad4268af751b
ZaharBerku <zaharberku@gmail.com>
2023-03-25 21:02:17 +0200

    added SectionTitle

---
+++ src/components/molecules/SectionTitle/index.tsx
@@ -0,0 +1,14 @@
+import type { FC } from "react";
+import { Box, Typography } from "@components/atoms";
+
+type SectionTitleProps = { title: string };
+
+const SectionTitle: FC<SectionTitleProps> = ({ title }) => {
+  return (
+    <Box className="py-12 text-center">
+      <Typography className="" text={title} tag="h3" />
+    </Box>
+  );
+};
+
+export { SectionTitle };
