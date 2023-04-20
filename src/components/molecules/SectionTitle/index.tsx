84f93f988d0ed5b287632b8ef2b0f08ac509a85c
ZaharBerku <zaharberku@gmail.com>
2023-04-21 01:37:14 +0300

    upgrade section title

--- src/components/molecules/SectionTitle/index.tsx
+++ src/components/molecules/SectionTitle/index.tsx
@@ -1,13 +1,16 @@
 import type { FC } from "react";
+import cx from "classnames";
 import { Box, Typography } from "@components/atoms";
+import type { SectionTitleProps } from "./index.types";
 
-type SectionTitleProps = { title: string };
-
-const SectionTitle: FC<SectionTitleProps> = ({ title }) => {
+const SectionTitle: FC<SectionTitleProps> = ({ title, className }) => {
   return (
     <Box className="py-6 md:py-12 text-center">
       <Typography
-        className="text-body-4xl inline-block after:block after:h-px2 after:w-full after:bg-white after:rounded-lg"
+        className={cx(
+          "text-body-4xl inline-block after:block after:h-px2 after:w-full after:bg-white after:rounded-lg",
+          className
+        )}
         text={title}
         tag="h3"
       />
---
+++ src/components/molecules/SectionTitle/index.types.ts
@@ -0,0 +1,1 @@
+export type SectionTitleProps = { title: string; className?: string };
