cf0e5b27c0aab70e1ca069c8601fbd50c67b03ab
ZaharBerku <zaharberku@gmail.com>
2023-06-10 10:16:01 +0300

    fix: size footer img

--- src/components/molecules/WrapperImg/index.constants.tsx
+++ src/components/molecules/WrapperImg/index.constants.tsx
@@ -3,9 +3,9 @@ import type { ClassesType } from "./index.types";
 export const classes: ClassesType = {
   circle: {
     wrapper:
-      "relative max-w-2xs h-2xs w-full flex-wrapper-column md:max-w-96 md:h-96",
-    box: "rounded-full w-full absolute top-0 bottom-0 border-warmGray-100 border -left-4",
-    img: "rounded-full max-w-xs md:max-w-2xs",
+      "relative max-w-xxs h-xxs w-full flex-wrapper-column md:max-w-96 md:h-96",
+    box: "rounded-full max-w-xxs md:max-w-full absolute top-0 bottom-0 right-0 border-warmGray-100 border -left-4",
+    img: "rounded-full max-w-xxs md:max-w-2xs",
   },
   square: {
     wrapper: "relative",
